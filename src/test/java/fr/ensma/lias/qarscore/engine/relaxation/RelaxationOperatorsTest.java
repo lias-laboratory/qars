39d5e1a613aa263de73a34cece094e3033f55070
Géraud FOKOU <geraud.fokou@ensma.fr>
2015-03-03 17:43:47 +0100

    test operator

---
+++ src/test/java/fr/ensma/lias/qarscore/engine/relaxation/RelaxationOperatorsTest.java
@@ -0,0 +1,127 @@
+/*********************************************************************************
+ * This file is part of QARS Project.
+ * Copyright (C) 2015  LIAS - ENSMA
+ *   Teleport 2 - 1 avenue Clement Ader
+ *   BP 40109 - 86961 Futuroscope Chasseneuil Cedex - FRANCE
+ * 
+ * QARS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QARS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with QARS.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************************/
+package fr.ensma.lias.qarscore.engine.relaxation;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Geraud FOKOU
+ */
+public class RelaxationOperatorsTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * Test method for {@link fr.ensma.lias.qarscore.engine.relaxation.implementation.RelaxationOperatorsImpl#specialization(fr.ensma.lias.qarscore.engine.query.CQuery, com.hp.hpl.jena.graph.Node, int)}.
+     */
+    @Test
+    public void testSpecializationCQueryNodeInt() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link fr.ensma.lias.qarscore.engine.relaxation.implementation.RelaxationOperatorsImpl#specialization(fr.ensma.lias.qarscore.engine.query.CQuery, com.hp.hpl.jena.graph.Node)}.
+     */
+    @Test
+    public void testSpecializationCQueryNode() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link fr.ensma.lias.qarscore.engine.relaxation.implementation.RelaxationOperatorsImpl#generalization(fr.ensma.lias.qarscore.engine.query.CQuery, com.hp.hpl.jena.graph.Node, int)}.
+     */
+    @Test
+    public void testGeneralizationCQueryNodeInt() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link fr.ensma.lias.qarscore.engine.relaxation.implementation.RelaxationOperatorsImpl#generalization(fr.ensma.lias.qarscore.engine.query.CQuery, com.hp.hpl.jena.graph.Node)}.
+     */
+    @Test
+    public void testGeneralizationCQueryNode() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link fr.ensma.lias.qarscore.engine.relaxation.implementation.RelaxationOperatorsImpl#generalize(fr.ensma.lias.qarscore.engine.query.CQuery, com.hp.hpl.jena.graph.Node, int)}.
+     */
+    @Test
+    public void testGeneralizeCQueryNodeInt() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link fr.ensma.lias.qarscore.engine.relaxation.implementation.RelaxationOperatorsImpl#generalize(fr.ensma.lias.qarscore.engine.query.CQuery, com.hp.hpl.jena.graph.Node)}.
+     */
+    @Test
+    public void testGeneralizeCQueryNode() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link fr.ensma.lias.qarscore.engine.relaxation.implementation.RelaxationOperatorsImpl#sibling(fr.ensma.lias.qarscore.engine.query.CQuery, com.hp.hpl.jena.graph.Node)}.
+     */
+    @Test
+    public void testSibling() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link fr.ensma.lias.qarscore.engine.relaxation.implementation.RelaxationOperatorsImpl#relaxValue(fr.ensma.lias.qarscore.engine.query.CQuery, com.hp.hpl.jena.graph.Node)}.
+     */
+    @Test
+    public void testRelaxValue() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link fr.ensma.lias.qarscore.engine.relaxation.implementation.RelaxationOperatorsImpl#releaseValue(fr.ensma.lias.qarscore.engine.query.CQuery, com.hp.hpl.jena.graph.Node)}.
+     */
+    @Test
+    public void testReleaseValue() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link fr.ensma.lias.qarscore.engine.relaxation.implementation.RelaxationOperatorsImpl#releaseJoin(fr.ensma.lias.qarscore.engine.query.CQuery, com.hp.hpl.jena.graph.Node)}.
+     */
+    @Test
+    public void testReleaseJoin() {
+	fail("Not yet implemented"); // TODO
+    }
+
+}
