8eae6818246748be1de19f59604f3a9aeff7130d
Géraud FOKOU <geraud.fokou@ensma.fr>
2015-11-27 00:05:58 +0100

    test ontology update

--- src/test/java/fr/ensma/lias/qarscore/connection/SessionTDBTest.java
+++ src/test/java/fr/ensma/lias/qarscore/connection/SessionTDBTest.java
@@ -46,16 +46,16 @@ public class SessionTDBTest extends InitTest {
     public Map<OntClass, Integer> class_instance;
     public Map<OntProperty, Integer> property_Triplet;
     public Logger logger = Logger.getLogger(SessionTDBTest.class);
-    
+
     @Before
     public void setUp() {
-	
+
 	super.setUp();
-//	Properties.setModelMemSpec(OntModelSpec.OWL_MEM);
-//	Properties.setOntoLang("OWL");
+	// Properties.setModelMemSpec(OntModelSpec.OWL_MEM);
+	// Properties.setOntoLang("OWL");
 	class_instance = new HashMap<OntClass, Integer>();
 	property_Triplet = new HashMap<OntProperty, Integer>();
-//	sessionJena = SessionFactory.getTDBSession(tdb_path);
+	// sessionJena = SessionFactory.getTDBSession(tdb_path);
     }
 
     @After
@@ -67,76 +67,84 @@ public class SessionTDBTest extends InitTest {
 	}
     }
 
-//    @Test
+    // @Test
     public void testSessionTDB() {
 
 	Assert.assertNotNull(((JenaSession) sessionJena).getDataset());
 	Assert.assertNotNull(((JenaSession) sessionJena).getModel());
 	Assert.assertNotNull(((JenaSession) sessionJena).getOntology());
 	Assert.assertTrue(((JenaSession) sessionJena).getTripleList().size() != 0);
-	logger.info(
-		((JenaSession) sessionJena).getDataset().toString());
+	logger.info(((JenaSession) sessionJena).getDataset().toString());
 
-	for (Resource classe : ((JenaSession) sessionJena).getInformation_content()
-		.keySet()) {
-	    logger.info(
-		    classe.getURI()
-			    + " has information content "
-			    + ((JenaSession) sessionJena).getInformation_content()
-				    .get(classe));
+	for (Resource classe : ((JenaSession) sessionJena)
+		.getInformation_content().keySet()) {
+	    logger.info(classe.getURI()
+		    + " has information content "
+		    + ((JenaSession) sessionJena).getInformation_content().get(
+			    classe));
 	}
     }
-    
+
     @Test
-    public void testOntologyTDB(){
+    public void testOntologyTDB() {
 	Assert.assertNotNull(((JenaSession) sessionJena).getDataset());
 	Assert.assertNotNull(((JenaSession) sessionJena).getModel());
 	OntModel ontology = ((JenaSession) sessionJena).getOntology();
 	Assert.assertNotNull(ontology);
-	
+
 	ExtendedIterator<OntClass> listClass = ontology.listClasses();
 	logger.info("SubClassOf");
 	while (listClass.hasNext()) {
 	    OntClass currentClass = listClass.next();
-	    class_instance.put(currentClass, currentClass.listInstances(false).toList().size());
-	    List<OntClass> subclasses = currentClass.listSubClasses(true).toList();
+	    class_instance.put(currentClass, currentClass.listInstances(false)
+		    .toList().size());
+	    List<OntClass> subclasses = currentClass.listSubClasses(true)
+		    .toList();
 	    while (!subclasses.isEmpty()) {
 		OntClass currentSubClass = subclasses.get(0);
 		subclasses.remove(currentSubClass);
-		logger.info(currentSubClass.getLocalName()+"-->"+currentClass.getLocalName());
+		logger.info(currentSubClass.getLocalName() + "-->"
+			+ currentClass.getLocalName());
 	    }
 	}
-	
+
 	logger.info("Instance by class");
-	for(OntClass key:class_instance.keySet()){
-	    logger.info(key.getLocalName()+":"+class_instance.get(key).intValue());
+	for (OntClass key : class_instance.keySet()) {
+	    logger.info(key.getLocalName() + ":"
+		    + class_instance.get(key).intValue());
 	}
-	
+
 	logger.info("All Instance Number");
 	logger.info(ontology.listIndividuals().toList().size());
-	
+
 	logger.info("SubpropertyOf");
-	ExtendedIterator<OntProperty> listProperty = ontology.listAllOntProperties();
+	ExtendedIterator<OntProperty> listProperty = ontology
+		.listAllOntProperties();
 
 	while (listProperty.hasNext()) {
 	    OntProperty currentProperty = listProperty.next();
-	    int number = ontology.listResourcesWithProperty(currentProperty, null).toList().size();
+	    int number = ontology
+		    .listResourcesWithProperty(currentProperty, null).toList()
+		    .size();
 	    List<OntProperty> subproperties = new ArrayList<OntProperty>();
 	    subproperties.addAll(currentProperty.listSubProperties().toList());
 	    while (!subproperties.isEmpty()) {
 		OntProperty currentSubProperty = subproperties.get(0);
 		subproperties.remove(currentSubProperty);
-		logger.info(currentSubProperty.getLocalName()+"-->"+currentProperty.getLocalName());
-		number = number + ontology.listResourcesWithProperty(currentSubProperty, null).toList().size();
+		if (!currentProperty.equals(currentSubProperty)) {
+		    logger.info(currentSubProperty.getLocalName() + "-->"+ currentProperty.getLocalName());
+		    number = number + ontology.listResourcesWithProperty(currentSubProperty, null).toList().size();
+		}
 	    }
-	    property_Triplet.put(currentProperty, number) ;  
+	    property_Triplet.put(currentProperty, number);
 	}
-	
+
 	logger.info("Triple by Property");
-	for( OntProperty key:property_Triplet.keySet()){
-	    logger.info(key.getLocalName()+":"+property_Triplet.get(key).intValue());
+	for (OntProperty key : property_Triplet.keySet()) {
+	    logger.info(key.getLocalName() + ":"
+		    + property_Triplet.get(key).intValue());
 	}
-	
+
 	logger.info("All Triple Number");
 	logger.info(ontology.listStatements().toList().size());
     }
