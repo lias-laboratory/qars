66eb20596bed18b396fb4a4727f51105644e6c9b
Géraud FOKOU <geraud.fokou@ensma.fr>
2015-12-04 15:01:26 +0100

    test huang query Q4TP-5

--- src/test/java/fr/ensma/lias/qarscore/benchmark/BenchmarkStrategiesTest.java
+++ src/test/java/fr/ensma/lias/qarscore/benchmark/BenchmarkStrategiesTest.java
@@ -657,7 +657,7 @@ public class BenchmarkStrategiesTest extends InitTest {
      * Experiments for LUBM *
      ************************/
 
-//    @Test
+    @Test
     public void testLUBM_Huang() throws Exception {
 
 	newTestResultPairList = this.newTestResultPairList("/"
@@ -773,7 +773,7 @@ public class BenchmarkStrategiesTest extends InitTest {
 	newResultExplain.generateReport();
     }
 
-    @Test
+//    @Test
     public void testLUBM_MFSFULLINC() throws Exception {
 
 	newTestResultPairList = this.newTestResultPairList("/"
