f667d48e86ba1873140727641cde16605515758d
Géraud FOKOU <geraud.fokou@ensma.fr>
2015-12-04 10:06:36 +0100

    mfs test

--- src/test/java/fr/ensma/lias/qarscore/benchmark/BenchmarkStrategiesTest.java
+++ src/test/java/fr/ensma/lias/qarscore/benchmark/BenchmarkStrategiesTest.java
@@ -657,7 +657,7 @@ public class BenchmarkStrategiesTest extends InitTest {
      * Experiments for LUBM *
      ************************/
 
-    @Test
+//    @Test
     public void testLUBM_Huang() throws Exception {
 
 	newTestResultPairList = this.newTestResultPairList("/"
@@ -717,7 +717,7 @@ public class BenchmarkStrategiesTest extends InitTest {
 	newResultExplain.generateReport();
     }
 
-    // @Test
+    @Test
     public void testLUBM_MFS() throws Exception {
 
 	newTestResultPairList = this.newTestResultPairList("/"
