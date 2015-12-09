d8d85fda8df4e46e547ef77c5b8efaab6f805985
Géraud FOKOU <geraud.fokou@ensma.fr>
2015-12-09 16:23:40 +0100

    mfs test

--- src/test/java/fr/ensma/lias/qarscore/benchmark/BenchmarkStrategiesTest.java
+++ src/test/java/fr/ensma/lias/qarscore/benchmark/BenchmarkStrategiesTest.java
@@ -1071,7 +1071,7 @@ public class BenchmarkStrategiesTest extends InitTest {
 	newResultExplain.generateReport();
     }
 
-//    @Test
+    @Test
     public void testLUBM_MFS() throws Exception {
 
 	newTestResultPairList = this.newTestResultPairList("/"
